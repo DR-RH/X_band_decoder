a39e8730f68f61b5eb628f45df27ccf8dca0a8a2
rh <kuma@kwansei.ac.jp>
2026-01-07 14:23:38 +0900

    programming complete

--- apps/app1_decode.py
+++ apps/app1_decode.py
@@ -4,72 +4,123 @@ from common import constants as C
 import datetime
 import os 
 import csv
+import pickle
+import shutil
 
 """
-次回すること
-カラム名をつける
+トータルパケットを追加する
+保存にタイプを記載する
 """
 
-def save_packet_group_file():
+def save_packet_group_file(data, file_uid, extension, output_dir='./output/X_band_decoded'):
+    os.makedirs(output_dir, exist_ok=True)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    file_created_time = datetime.datetime.fromtimestamp(int(file_uid,16)).strftime("%Y%m%d%H%M%S")
+    filename = f'{output_dir}/decoded_{file_created_time}_{timestamp}.{extension}'
+    with open(filename, 'wb') as f:
+        f.write(data)
+    print(f"Saved {extension} file for UID {file_uid} as: {filename}")
+
+    print(f"decoded :{filename}")
+    return filename
     return
 
 def move_files(files):
     print(f"move file {files}" )
+    for file in files:
+        filename = file.split('/')[-1]
+        dst_filename = f'data/raw_data_processed/{filename}'
+        shutil.move(file, dst_filename)
     return
 
+def initialize_report_file():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    os.makedirs('output/report',exist_ok=True)
+    path_w = f'output/report/{timestamp}.csv'
+    with open(path_w, mode='w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["ID","start","end","number"])
+    return path_w
+
+def save_loss_packtet_group(packet_loss_group,):
+    os.makedirs('data/loss_packet_group',exist_ok=True)
+    with open('data/loss_packet_group/loss_packet_group.pkl', 'wb') as f:
+        pickle.dump(packet_loss_group, f)
+    return
+
+def load__loss_packtet_group():
+
+    path = 'data/loss_packet_group/loss_packet_group.pkl'
+    if not os.path.exists(path):
+        return {}
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+    
+
+def reassemble_packet_group(packet_group, extension):
+    packet_data = packet_group['packets']
+    combined_packet = b"".join(packet_data)
+
+    if extension == 'bin':
+        reassembled_data = combined_packet[:3003*3008*2] # end data handling
+    else:
+        reassembled_data = bytes(combined_packet).rstrip(b'\0')
+
+    return reassembled_data
+
+def check_extension(ptype):
+    if ptype == 0x03:
+        extension = 'txt'
+    elif ptype == 0x04:
+        extension = 'log'
+    elif ptype == 0x01:
+        extension = 'csv'
+    elif ptype == 0x05:
+        extension = 'jpg'
+    else:
+        extension = 'bin'
+    return extension
 
 def main(target_files):
 
-    packet_groups = {}
+    packet_groups = load__loss_packtet_group()
     # packet_group =  read_packet_group()
     for target_file in target_files:
         print(f"Decoding file: {target_file}")
         packet_groups = decode_utils.decode_packets(target_file, packet_groups)
 
+    packet_loss_group = {} 
     for k, v in packet_groups.items():
         id = k
-        packets = v['packets']
+        packet_group = v
+        packets = packet_group['packets']
+        ptype = packet_group['ptypes'][0]
+        extension = check_extension(ptype)
+
         print(f'ID is {id}')
         loss_sequence = []
         for i, packet in enumerate(packets):
             if packet == bytes([0xEE])*C.PAYLOAD_SIZE:
-                # print(i/)
                 print(f'find packet loss @ {i}')
                 loss_sequence.append(i)
-        print(loss_sequence)
-        if loss_sequence:
-            print('write')
-            timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            os.makedirs('output/report',exist_ok=True)
-            path_w = f'output/report/{timestamp}.csv'
+        path_w = initialize_report_file()
+        if not loss_sequence:
+            reassembled_data = reassemble_packet_group(packet_group, extension)
+            save_packet_group_file(reassembled_data, id, extension)
+        else:
+            print('loss packet found ')
             ranges = decode_utils.get_ranges(loss_sequence)
-            print(ranges[0])
-            with open(path_w, mode='w') as f:
-                # f.write(ranges[0])
+            with open(path_w, mode='a') as f:
                 writer = csv.writer(f)
                 for range in ranges:
                     writer.writerow([id]+list(range))
+            packet_loss_group[k] = v
+
+    save_loss_packtet_group(packet_loss_group)
 
 
 
-    # for file_uid, packet_group in packet_groups.items():
-    #     reassembled_data, missing_seq, extension = reassemble_packet_group(packet_group)
-    #     total_packet_size = packet_group['total_packet_size']
-    #     calculated_file_size = total_packet_size * PAYLOAD_SIZE  # Total file size in bytes based on packet count.
 
-    #     print(f"\nUID: {file_uid} | Type: {extension}")
-    #     print(f"Number of Packets (from MDPU header): {total_packet_size}")
-    #     print(f"Calculated File Size (from packet count): {calculated_file_size} bytes")
-
-    #     if missing_seq:
-    #         ranges = get_ranges(missing_seq)
-    #         print("Missing packet sequence numbers (start, end, loss numbers):", (ranges))
-    #         print(f"Missing packet numbers: {len(missing_seq)}")
-    #         #save_with_missing_seq()
-    #     else:
-    #         print("No missing packets.")
-    #         save_packet_group_file(reassembled_data, file_uid, extension)
-    
     move_files(target_files)
 
     return 
@@ -81,10 +132,11 @@ if __name__ == "__main__":
     # target_files = glob.glob('x_band_test_data/x_band_test_data/four_images_F20250515170302.bin')
     # target_files = glob.glob('x_band_test_data/part*.bin')
   
-    files = glob.glob("data/x_devided_bin/*.bin")
+    files = glob.glob("data/raw_data_unprocessed"
+    "/*.bin")
     target_files = [f for f in files if "packet_base" not in f]
 
-    # target_files = glob.glob("devide_bin/*packet_base*.bin")
+    # target_files = glob.glob("data/x_divided_bin/*packet_base*.bin")
     target_files.sort(reverse=False)
     target_files = target_files
     # target_files = target_files[:6]
